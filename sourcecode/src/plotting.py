50053b2244d0cd4cb73b728e13d0c8a11d920d61
chips <ChipsSpectre>
2019-04-21 22:39:51 +0200

    implement plotting of loss over time.

--- sourcecode/src/plotting.py
+++ sourcecode/src/plotting.py
@@ -6,6 +6,7 @@ import numpy as np
     Contains scripts to plot different plots of the report.
 """
 
+
 def plot_prediction(data, train_pred, val_pred):
     n_train, n_val = len(train_pred), len(val_pred)
     n_data = len(data)
@@ -15,4 +16,13 @@ def plot_prediction(data, train_pred, val_pred):
     plt.plot((n_data - n_val) + np.arange(n_val), val_pred, color="red")
     plt.legend(("ground truth", "prediction (train)", "prediction (val)"))
 
+    plt.show()
+
+
+def plot_trainval_loss(history, sigma):
+    N = len(history.history["loss"])
+    plt.plot(1 + np.arange(N), history.history["loss"])
+    plt.plot(1 + np.arange(N), history.history["val_loss"])
+    plt.plot(1 + np.arange(N), np.ones(N) * (sigma * sigma))
+    plt.legend(("train", "val", "variance"))
     plt.show()
--- sourcecode/src/sine_prediction.py
+++ sourcecode/src/sine_prediction.py
@@ -1,9 +1,10 @@
+import matplotlib.pyplot as plt
 import numpy as np
 
 from data_generator import DataGenerator
 from models import TwoLayerNet
 from noise import iid_normal_noise
-from plotting import plot_prediction
+from plotting import plot_prediction, plot_trainval_loss
 from utils import generate_sequences, train_val_split
 
 """
@@ -15,23 +16,25 @@ if __name__ == "__main__":
     in_seq_len = 3
     out_seq_len = 1
     sigma = 0.1
+    epochs = 100
 
-    data_points = DataGenerator.generate_sine(N=5000, start=0, end=2 * np.pi)
+    data_points = DataGenerator.generate_sine(N=1000, start=0, end=4 * np.pi)
     data_points = data_points + iid_normal_noise(data_points) * sigma
 
     train_data, val_data = train_val_split(data_points, val_fraction=0.2)
     train_x, train_y = generate_sequences(train_data, in_seq_len, out_seq_len)
     val_x, val_y = generate_sequences(val_data, in_seq_len, out_seq_len)
 
-    net = TwoLayerNet(n_input=in_seq_len, n_hidden=100, n_output=out_seq_len)
+    net = TwoLayerNet(n_input=in_seq_len, n_hidden=10, n_output=out_seq_len)
     net.compile("adam", "mse")
 
-    history = net.train(train_x, train_y, val_x, val_y, epochs=100)
+    history = net.train(train_x, train_y, val_x, val_y, epochs=epochs)
 
     train_prediction = net.predict(train_x)
     val_prediction = net.predict(val_x)
 
     # plot results
     plot_prediction(data_points, train_prediction, val_prediction)
-    n_train, n_val = train_x.shape[0], val_x.shape[0]
-    n_data = len(data_points)
+
+    # plot the loss compared to variance of the noise
+    plot_trainval_loss(history, sigma)
