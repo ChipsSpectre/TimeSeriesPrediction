0e044358b1b1ce6c350c5a921a3f4d3ac11ff7fe
ChipsSpectre <maximilian.hornung@tum.de>
2019-04-23 19:22:04 +0200

    first time series production using LSTM.

--- sourcecode/src/tyson_oscillator_simulator.py
+++ sourcecode/src/tyson_oscillator_simulator.py
@@ -3,6 +3,7 @@ import gillespy
 import scipy as sp
 import numpy as np
 import matplotlib.pyplot as plt
+import time
 
 import sys
 sys.path[:0] = ['..']
@@ -16,9 +17,38 @@ if __name__ == '__main__':
     # =============================================
     # To set up the model, first create an empty model object. Then, add
     # species and parameters as was set up above.
+    t1 = time.time()
     tyson_trajectories = tyson_model.run(show_labels=False, seed=0)
+    t2 = time.time()
+    print("finished running in {} seconds.".format(t2-t1))   
     print(tyson_trajectories[0], tyson_trajectories[0].shape)
-    print("finished running.")
+
+    from keras.models import Sequential
+    from keras.layers import LSTM
+
+    n_units = 101
+    net = Sequential()
+    net.add(LSTM(units=n_units, return_sequences=True))
+    net.add(LSTM(units=n_units, return_sequences=False))
+    net.compile("adam", "mse")
+    x = np.array([2.0, 20.0, 1.0, 0.005, 0.05, 0.1, 1.0])
+    x = x.reshape([1, len(x), 1])
+    y = tyson_trajectories[0][:,1]
+    # normalize y
+    y = (y - np.min(y)) / (np.max(y) - np.min(y))
+    y = y.reshape([1, len(y)])
+    
+    net.fit(x, y, epochs=100)
+
+    y = y[0]
+
+    print(y)
+    pred = net.predict(x)
+    pred = pred[0]
+    print(pred)
+    plt.plot(np.arange(len(pred)), pred)
+    plt.plot(np.arange(len(y)), y)
+    plt.show()
 
     # =============================================
     # plot just the first trajectory, 0, in both time and phase space:
