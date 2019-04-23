f416625e061dc97f2d8dba6dcb5c79042ab40e0e
ChipsSpectre <maximilian.hornung@tum.de>
2019-04-23 20:10:35 +0200

    state fundamental limit for deterministic neural networks.

--- sourcecode/src/tyson_oscillator_simulator.py
+++ sourcecode/src/tyson_oscillator_simulator.py
@@ -4,12 +4,18 @@ import scipy as sp
 import numpy as np
 import matplotlib.pyplot as plt
 import time
+from keras.models import Sequential
+from keras.layers import LSTM
 
 import sys
+import os
 sys.path[:0] = ['..']
 
 if __name__ == '__main__':
     np.random.seed(0)
+    n_traj = 101
+    figure_path = os.path.join("..", "report", "figures")
+
     tyson_model = Tyson2StateOscillator()
 
     # =============================================
@@ -18,37 +24,52 @@ if __name__ == '__main__':
     # To set up the model, first create an empty model object. Then, add
     # species and parameters as was set up above.
     t1 = time.time()
-    tyson_trajectories = tyson_model.run(show_labels=False, seed=0)
+    tyson_trajectories = tyson_model.run(show_labels=False, seed=0,
+                                         number_of_trajectories=n_traj)
     t2 = time.time()
-    print("finished running in {} seconds.".format(t2-t1))   
-    print(tyson_trajectories[0], tyson_trajectories[0].shape)
-
-    from keras.models import Sequential
-    from keras.layers import LSTM
+    print("finished running in {} seconds.".format(t2-t1))
 
     n_units = 101
     net = Sequential()
     net.add(LSTM(units=n_units, return_sequences=True))
     net.add(LSTM(units=n_units, return_sequences=False))
     net.compile("adam", "mse")
-    x = np.array([2.0, 20.0, 1.0, 0.005, 0.05, 0.1, 1.0])
-    x = x.reshape([1, len(x), 1])
-    y = tyson_trajectories[0][:,1]
+    params = np.array([2.0, 20.0, 1.0, 0.005, 0.05, 0.1, 1.0])
+    x = np.zeros([n_traj-1, len(params), 1])
+    for i in range(n_traj-1):
+        x[i, :, 0] = params
+
+    y = np.zeros([n_traj-1, n_units])
+    for i in range(n_traj-1):
+        y[i, :] = tyson_trajectories[i][:, 1]
     # normalize y
     y = (y - np.min(y)) / (np.max(y) - np.min(y))
-    y = y.reshape([1, len(y)])
-    
+
     net.fit(x, y, epochs=100)
 
-    y = y[0]
-
-    print(y)
+    t1 = time.time()
     pred = net.predict(x)
+    t2 = time.time()
+    print("LSTM prediction done in {} seconds".format(t2-t1))
     pred = pred[0]
-    print(pred)
+
+    y_test = tyson_trajectories[n_traj-1][:, 1]
+    # normalize y
+    y_test = (y_test - np.min(y_test)) / (np.max(y_test) - np.min(y_test))
+
+    y_mean = np.mean(y, axis=0)
+
+    plt.plot(np.arange(len(y_test)), y_test)
     plt.plot(np.arange(len(pred)), pred)
-    plt.plot(np.arange(len(y)), y)
-    plt.show()
+    plt.plot(np.arange(len(y_mean)), y_mean)
+    plt.title("Stochastic time series generation")
+    plt.xlabel("time step")
+    plt.ylabel("time series value X(t)")
+    plt.legend(("ground truth", "prediction", "mean"))
+    plt.savefig(os.path.join(figure_path, "nn_limitation.pdf"))
+
+    import sys
+    sys.exit(0)
 
     # =============================================
     # plot just the first trajectory, 0, in both time and phase space:
