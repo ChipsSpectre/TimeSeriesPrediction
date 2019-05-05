6ea97fadac6f638c9da6fb36b1f642134819e1c1
Maximilian Johann Hornung <mjhor@kth.se>
2019-05-05 22:33:26 +0200

    refine the report.

--- sourcecode/src/mackey_glass_phaseplot.py
+++ sourcecode/src/mackey_glass_phaseplot.py
@@ -22,7 +22,7 @@ def plot_phaseplot(tau_nonchaotic, tau_chaotic):
     data_nonchaotic = get_mg_data(N, M, tau=tau_nonchaotic)
     plt.clf()
     plt.xlabel("x(t)")
-    plt.ylabel("x(t-τ)")
+    plt.ylabel("x(t-tau)")
     plt.title("Phase plot")
     plt.plot(data_nonchaotic[:-tau_nonchaotic],
              data_nonchaotic[tau_nonchaotic:], color="red")
@@ -31,14 +31,14 @@ def plot_phaseplot(tau_nonchaotic, tau_chaotic):
                                            gamma=0.1, n=10, tau=tau_chaotic)[tau_chaotic:]
     data = data[M:]
     plt.plot(data[:-tau_chaotic], data[tau_chaotic:], color="blue")
-    plt.legend(("τ=15", "τ=17"))
+    plt.legend(("tau=15", "tau=17"))
     plt.savefig(os.path.join(figure_path, "{}.pdf".format(tau_chaotic)))
     plt.show()
 
 
 if __name__ == "__main__":
     N = 10000
-    M = 9800
+    M = 9500
     figure_path = os.path.join("..", "report", "figures")
     tau_nonchaotic = 15
     plot_phaseplot(tau_nonchaotic=tau_nonchaotic, tau_chaotic = 17)
